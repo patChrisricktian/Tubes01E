adb2a3740c02c580e9f970bc09f906421f6f6fdd
ayogaks <ayogaks@gmail.com>
2020-10-23 14:13:37 +0700

    Penambahan isi Main Fragment

--- app/src/main/java/com/example/tubes01e/MainFragment.java
+++ app/src/main/java/com/example/tubes01e/MainFragment.java
@@ -14,13 +14,17 @@ import androidx.fragment.app.Fragment;
 import com.example.tubes01e.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
-    private TextView tvText;
+    private TextView tvText1;
+    private TextView tvText2;
+    private Button btn;
     private FragmentListener listener;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         FragmentMainBinding binding = FragmentMainBinding.inflate(getLayoutInflater());
         final View view = binding.getRoot();
-        this.tvText = binding.tvMain;
+        this.tvText1 = binding.tvTitle1;
+        this.tvText2 = binding.tvTitle2;
+        this.btn = binding.btnCari;
         return view;
     }
 
